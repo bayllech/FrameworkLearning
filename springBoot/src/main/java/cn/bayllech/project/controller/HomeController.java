f967145f046f75871189cbfd17cec49dc8bc015c
bayllech <hpuqibei@163.com>
2017-08-11 15:59:10 +0800

    配置使其支持jsp，主要在于prefix的路径问题上

--- springBoot/src/main/java/cn/bayllech/project/controller/HomeController.java
+++ springBoot/src/main/java/cn/bayllech/project/controller/HomeController.java
@@ -1,9 +1,11 @@
 package cn.bayllech.project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,15 +13,22 @@ import org.springframework.web.servlet.ModelAndView;
  * @Author bei.qi
  * @Date 2017-08-10 15:10
  */
-@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping()
+    @ResponseBody
     public String home() {
-        ModelMap model = new ModelMap();
-        model.addAttribute("msg", "1");
-
         return "Hello SpringBoot";
+    }
+
+    @RequestMapping("/jsp")
+    public String vieJsp(ModelMap model) {
+        /*model.addAttribute("code", "000000");
+        model.addAttribute("msg", "成功");*/
+        model.addAttribute("msg", "success");
+        System.out.println("/jsp.");
+        return "index";
     }
 
 }
