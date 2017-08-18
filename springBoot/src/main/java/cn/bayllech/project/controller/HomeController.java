0f8510f5c14332b732ced40dfd8b87a8ab7630f3
bayllech <hpuqibei@163.com>
2017-08-18 09:37:03 +0800

    timestamp转为date,页面显示正常，修改spring.jack.date设置，或者使用Gson转换格式

--- springBoot/src/main/java/cn/bayllech/project/controller/HomeController.java
+++ springBoot/src/main/java/cn/bayllech/project/controller/HomeController.java
@@ -4,11 +4,14 @@ import cn.bayllech.project.pojo.DUser;
 import cn.bayllech.project.pojo.TUserInfo;
 import cn.bayllech.project.service.DUserService;
 import cn.bayllech.project.service.TUserInfoService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -60,6 +63,8 @@ public class HomeController {
     public ModelMap dubboUser() {
         ModelMap modelMap = new ModelMap();
         List<DUser> userList = dUserService.findAllUser();
+//        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+//        return gson.toJson(userList);
         modelMap.addAttribute("userList", userList);
         return modelMap;
     }
