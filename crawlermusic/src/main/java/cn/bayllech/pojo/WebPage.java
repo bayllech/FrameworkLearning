6851ba4449b839280eb77a3b4a2f0b8b385a9eb4
bayllech <hpuqibei@163.com>
2017-09-08 14:17:35 +0800

    新建网易云爬虫项虫项目

---
+++ crawlermusic/src/main/java/cn/bayllech/pojo/WebPage.java
@@ -0,0 +1,19 @@
+package cn.bayllech.pojo;
+
+/**
+ * @Author bei.qi
+ * @Description 网页爬取的实例
+ * @Date 2017-09-08 13:53
+ */
+public class WebPage {
+
+    public enum PageType{
+        song,playlist,playlists
+    }
+
+    public enum Status{
+        crawled,uncrawled
+    }
+
+
+}
