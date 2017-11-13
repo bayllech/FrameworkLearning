ba081ae0bcb5a4172bea47307804932e9e015226
bayllech <hpuqibei@163.com>
2017-11-13 10:48:30 +0800

    dubbo consumer 调用成功

---
+++ demo-consumer/src/main/java/app.java
@@ -0,0 +1,19 @@
+import cn.bayllech.service.UserInfoService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Author bei.qi
+ * @Description
+ * @Date 2017-11-13 10:33
+ */
+public class app {
+    
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        context.start();
+        UserInfoService userInfoService = (UserInfoService) context.getBean("userInfoServiceImpl");
+        System.out.println("dubbo consumer 调用成功： " + userInfoService.selectTotal(0,10));
+        
+    }
+}
---
+++ demo-consumer/src/main/java/cn/bayllech/service/UserInfoService.java
@@ -0,0 +1,12 @@
+package cn.bayllech.service;
+
+/**
+ * @Author bei.qi
+ * @Description
+ * @Date 2017-11-01 10:30
+ */
+public interface UserInfoService {
+    
+    Long selectTotal(Integer offset, Integer limit);
+    
+}
