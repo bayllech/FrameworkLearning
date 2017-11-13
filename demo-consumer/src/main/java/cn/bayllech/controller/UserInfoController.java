981cf6c33e88c09be17577fc50183556bbb0911c
bayllech <hpuqibei@163.com>
2017-11-13 14:09:20 +0800

    dubo start

---
+++ demo-consumer/src/main/java/cn/bayllech/controller/UserInfoController.java
@@ -0,0 +1,20 @@
+package cn.bayllech.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author bei.qi
+ * @Description
+ * @Date 2017-11-13 11:01
+ */
+@Controller
+@RequestMapping("info")
+public class UserInfoController {
+
+
+
+}
