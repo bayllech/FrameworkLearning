67611d3e238442d907589edc0b684e639bdfb4ec
bayllech <hpuqibei@163.com>
2017-11-29 16:17:08 +0800

    添加Hystrix断路器，eureka clent loadBalanced 采用的是轮换模式，并不是根据是否可用随机分配

--- springcloud-serviceribbon/src/main/java/cn/bayllech/springcloudserviceribbon/service/HelloService.java
+++ springcloud-serviceribbon/src/main/java/cn/bayllech/springcloudserviceribbon/service/HelloService.java
@@ -1,5 +1,6 @@
 package cn.bayllech.springcloudserviceribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,8 +15,13 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
     
+    @HystrixCommand(fallbackMethod = "helloError")
     public String hello(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name=" + name, String.class);
     
+    }
+    
+    public String helloError(String name) {
+        return "hello " + name + "错误";
     }
 }
