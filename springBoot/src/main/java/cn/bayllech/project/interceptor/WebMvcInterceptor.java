ef4f309d3edbbd517cb853b7bd6f915ef8982fd5
bayllech <hpuqibei@163.com>
2017-12-07 20:07:27 +0800

    修改拦截器

--- springBoot/src/main/java/cn/bayllech/project/interceptor/WebMvcInterceptor.java
+++ springBoot/src/main/java/cn/bayllech/project/interceptor/WebMvcInterceptor.java
@@ -32,7 +32,7 @@ public class WebMvcInterceptor extends WebMvcConfigurerAdapter {
     public void addInterceptors(InterceptorRegistry registry) {
         // addPathPatterns 用于添加拦截规则
         // excludePathPatterns 用户排除拦截
-        registry.addInterceptor(new MyInterceptor()).addPathPatterns("/leaf/**"); //.excludePathPatterns("/toLogin","/leaf/login");
+        registry.addInterceptor(new MyInterceptor()).addPathPatterns("/leaf/**").excludePathPatterns("/leaf/login"); //.excludePathPatterns("/toLogin","/leaf/login");
         super.addInterceptors(registry);
     }
 }
