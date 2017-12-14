39b005bdd6dd780a1f5d7559816890cb88d63cff
bayllech <hpuqibei@163.com>
2017-12-14 13:17:04 +0800

    使用Spring Cloud Bus与Spring Cloud Config的整合，并以RabbitMQ作为消息代理，实现了应用配置的动态更新

--- springcloud-config-client/src/main/java/cn/bayllech/springcloudconfigclient/SpringcloudConfigClientApplication.java
+++ springcloud-config-client/src/main/java/cn/bayllech/springcloudconfigclient/SpringcloudConfigClientApplication.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableDiscoveryClient
+@RefreshScope  //bus rabbitmq 请求client任意一端口的/bus/refresh，消息总线发送消息，更新所有配置
 public class SpringcloudConfigClientApplication {
 
 	public static void main(String[] args) {
